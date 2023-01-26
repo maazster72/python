0913348dd2003817812b809b442ff9c1ccbe58a2
maazster72 <mabdulahmed02@gmail.com>
2023-01-26 22:45:17 +0000

    developed frame_roll

--- 10_pin_bowling/bowling.py
+++ 10_pin_bowling/bowling.py
@@ -20,7 +20,7 @@ def set_players():
     players = []
     for i in range(no_of_players):
         player = input("Please enter name of player %s: " % str(i+1))
-        player = {"name":player,"score":0}
+        player = {"name":player,"score":0,"strike": False,"spare":False}
         players.append(player)
     return players
 
@@ -55,17 +55,49 @@ def display_player_turn(player):
     string = '\n* {}\'s turn *'
     string = string.format(player_name)
     print(string)
+
+def check_valid_score(score):
+    if(score < 0):
+        return False
+    else:
+        return True
     
-
-
 def ball(num):
     string = "Ball {}"
     print(string.format(num))
-    score = input("Enter number of pins knocked down: ")
+    score = int(input("Enter number of pins knocked down: "))
+    while(check_valid_score(int(score)) != True):
+        score = int(input("Please enter a valid number: "))
     return score
+
+def frame_normal_roll(player):
+    strike = player['strike']
+    spare = player['spare']
+
+    score = ball('1')
+    if(score != 10):
+        score += ball('2')
+        if(score == 10):
+            spare = True
+    else:
+        strike = True
+    frame_roll = {'score':score,'strike':strike,'spare':spare}
+    return frame_roll
+
+def frame(num, players):
+    display_frame(num)
+    for j in range(len(players)):
+        display_player_turn(players[j])
+        frame_roll = frame_roll()
 
 def start_game():
     print("\n* Welcome to 10-pin bowling! *\n")
+
+# def frame(player, frame, bonus_strike,bonus_spare,score_prev_round):
+#     if(frame != 10):
+#         if(bonus_strike):
+#             score = score_prev_round
+#             for i in range
 
 def main():
     start_game()
@@ -88,4 +120,5 @@ def main():
 
 
 # print(set_players(set_no_of_players()))
-main()
+#main()
+print(frame_normal_roll())
